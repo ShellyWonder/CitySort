604691d667662a048157c7edacf56a71a878ce92
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-09-05 21:26:18 -0500

    added code for population function

--- js/site.js
+++ js/site.js
@@ -126,7 +126,8 @@ let cityData = [{
 function citySort() {
 
     
-    sortByPopulation(cityData, "desc");
+  sortByPopulationDSC(cityData, "desc");
+  sortByPopulationASC(cityData, "asc");
     
     //extra credit functions
     //sortyByName(cityData);
@@ -137,7 +138,7 @@ function citySort() {
     let trow = "";
     //display the data 
     cityData.forEach(item => {
-        trow += `<tr><td>${item.city}</td><td>${item.state_code}</td><td>${item.population.toLocaleString("en-US")}</td><td>${item.median_age}</td><td>${item.avg_household_size}</td></tr>`;
+        trow += `<tr><td class="text-center">${item.city}</td><td td class="text-center">${item.state_code}</td><td td class="text-center">${item.population.toLocaleString("en-US")}</td><td td class="text-center">${item.median_age}</td><td td class="text-center">${item.avg_household_size}</td></tr>`;
     });
     tbody.innerHTML = "";
     tbody.innerHTML = trow;
@@ -145,15 +146,27 @@ function citySort() {
 
 //takes an array of objects and sorts by population. 
 function sortByPopulation(cityData, sortDir) {
-    
+  //numeric sort--anonymous function to compare  two objects//then reverse & compare again 
+  //positive number, negative number or zero; zero = no action needed
+  cityData.sort((a, b) => {
+    if (sortDir == "asc") {
+      return (a.population - b.population);//which one is larger 
+    } else {
+      //descending order
+      return (b.population - a.population);//which one is larger 
+      
+    }
+  });
 }
 
-//takes an array of objects and sorts by median age. 
+
+
+//Sorts array by median age. 
 function sortByAge(cityData, sortDir){
   
 }
 
-//takes an array of objects and sorts by city name. 
+//Sorts array by city name. 
 function sortyByName(cityData) {
     
 }
